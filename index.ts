02b296c53724f22b2996c21fd42a15b42a48f6f9
SibusisoNgubeni <156700279+SibusisoNgubeni@users.noreply.github.com>
2024-06-10 15:59:03 +0200

    reviewTotalDisplay' is possibly 'null'

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@
 // Write a function that will only accept numbers and attend to 
 // all TypeScript weakness flags.
 // : number
+
 const reviewTotalDisplay = document.querySelector('#reviews')
 
 const reviews = [
@@ -24,3 +25,10 @@ const reviews = [
         date: '27-03-2021'
     },
 ]
+
+// Solution
+function showReviewTotal (value : number) {
+    reviewTotalDisplay.innerHTML = 'review total ' + value.toString()
+}
+
+showReviewTotal(reviews.length)
