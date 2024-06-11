f8fa5fd9c8960ece828c5a274f252f3e5638362f
SibusisoNgubeni <156700279+SibusisoNgubeni@users.noreply.github.com>
2024-06-11 16:01:26 +0200

    funtction types

--- index.ts
+++ index.ts
@@ -1,32 +1,25 @@
-// Number Types mini-challenge 10 10.2
-// Write a function that will only accept numbers and attend to 
-// all TypeScript weakness flags.
-// : number
+import { showReviewTotal, populateUser } from './utils';
+import { Permissions, LoyaltyUser } from './enums';
+import { Price, Country } from './types';
 
-const propertyContainer = document.querySelector('.properties')
-import { showReviewTotal, populateUser } from './utils'
-import { Permissions , LoyaltyUser } from './Enums'
-const footer = document.querySelector('.footer')
+const propertyContainer = document.querySelector('.properties') as HTMLDivElement;
+const footer = document.querySelector('.footer') as HTMLDivElement;
 
-
-let isOpen: boolean
+let isOpen: boolean;
 
 // Reviews
-const reviews : (
-    {
+const reviews: ({
     name: string;
     stars: number;
     loyaltyUser: LoyaltyUser;
     date: string;   
-} |
-{
+} | {
     name: string;
     stars: number;
     loyaltyUser: LoyaltyUser;
     date: string;
     description: string;
-}
-)[]= [
+})[] = [
     {
         name: 'Sheia',
         stars: 5,
@@ -44,100 +37,107 @@ const reviews : (
         stars: 4,
         loyaltyUser: LoyaltyUser.SILVER_USER,
         date: '27-03-2021'
-    },
-]
+    }
+];
 
 // User
 const you: {
     firstName: string;
     lastName: string;
-    permissions: Permissions.ADMIN,
-    isReturning: true;
+    permissions: Permissions;
+    isReturning: boolean;
     age: number;
-    stayedAt: string[]
+    stayedAt: string[];
 } = {
     firstName: 'Bobby',
     lastName: 'Brown',
+    permissions: Permissions.ADMIN,
     isReturning: true,
     age: 35,
     stayedAt: ['florida-home', 'oman-flat', 'tokyo-bungalow']
-}
+};
 
 // Array of Properties
-const properties : {
+const properties: {
     image: string;
     title: string;
-    price: number;
+    price: Price;
     location: {
         firstLine: string;
         city: string;
         code: number;
-        country: string;
+        country: Country;
     };
-
-    //contact was made into a Tuple type
-    //A tuple is a collection of values that can be of different types, such as numbers, strings, booleans, and other types.
     contact: [number, string];
     isAvailable: boolean;
-
 }[] = [
     {
         image: 'images/cashan1.jpg',
         title: '2 story',
-        price: 90,
+        price: 45,
         location: {
             firstLine: 'house 37',
             city: 'Rustenburg',
             code: 45632,
-            country: 'South africa'
+            country: Country.South_Africa
         },
         contact: [+1123495082908, 'marywinkle@gmail.com'],
-        isAvailable: true  
+        isAvailable: true
     },
     {
         image: 'images/cashan2.jpg',
         title: 'cashan villa',
-        price: 101,
+        price: 25,
         location: {
             firstLine: 'house 23',
             city: 'ibiza',
             code: 343903,
-            country: 'spain'
+            country: Country.Spain
         },
         contact: [+1123495082908, 'garydavis@hotmail.com'],
-        isAvailable: false 
+        isAvailable: false
     },
     {
-        image: 'images/cashan3jpg',
+        image: 'images/cashan3.jpg',
         title: 'cashan Flat',
-        price: 23,
+        price: 30,
         location: {
             firstLine: 'flat 15',
             city: 'cairo',
             code: 35433,
-            country: 'Egypt',
+            country: Country.Egypt
         },
-        contact: [ +1123495082908, 'andyluger@aol.com'],
+        contact: [+1123495082908, 'andyluger@aol.com'],
         isAvailable: true
     }
-]
-
+];
 
 // Functions
-showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser)
+showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser);
+populateUser(you.isReturning, you.firstName);
 
-populateUser(you.isReturning, you.firstName)
+let authorityStatus: boolean | Permissions;
+let isLoggedIn: boolean = false;
+
+function showDetails(authorityStatus: boolean | Permissions, element: HTMLDivElement, price: number) {
+    if (authorityStatus) {
+        const priceDisplay = document.createElement('div');
+        priceDisplay.innerHTML = price.toString() + '/night';
+        element.appendChild(priceDisplay);
+    }
+}
 
 // Add the properties
 for (let i = 0; i < properties.length; i++) {
-    const card = document.createElement('div')
-    card.classList.add('card')
-    card.innerHTML = properties[i].title
-    const image = document.createElement('img')
-    image.setAttribute('src', properties[i].image)
-    card.appendChild(image)
-    propertyContainer.appendChild(card)
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.innerHTML = properties[i].title;
+    const image = document.createElement('img');
+    image.setAttribute('src', properties[i].image);
+    card.appendChild(image);
+    propertyContainer.appendChild(card);
+    showDetails(you.permissions, card, properties[i].price);
 }
 
-let currentLocation: [string, string, number] = ['Rustenburg', '12:10', 17]
-footer.innerHTML = currentLocation[0] + ' ' + currentLocation[1] + ' ' + currentLocation[2] + '°'
+let currentLocation: [string, string, number] = ['Rustenburg', '12:10', 17];
+footer.innerHTML = `${currentLocation[0]} ${currentLocation[1]} ${currentLocation[2]}°`;
